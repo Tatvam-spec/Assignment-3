3e0750b89645ee99bbbc8f3d190574331afb96fa
Tatvam-spec <73544616+Tatvam-spec@users.noreply.github.com>
2025-10-02 22:38:56 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,6 @@
+# Task 2
+import math
+n=int(input("Enter a number: "))
+print("Square root of the number is: ", math.sqrt(n))
+print("Logarithm (base 10) of the number is: ", math.log(n))
+print("sine of the number is: ", math.sin(n))
